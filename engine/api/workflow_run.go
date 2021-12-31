41def71542e3647ef4ea429a908ead4638b973e6
Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>
2021-12-31 16:35:22 +0100

    feat: delete old action Serve Static File
    This action can be replaced by a standard action using an integration
    (artifactory integration for example)
    
    Signed-off-by: Yvonnick Esnault <yvonnick.esnault@corp.ovh.com>

--- engine/api/workflow_run.go
+++ engine/api/workflow_run.go
@@ -805,7 +805,6 @@ func (api *API) getWorkflowNodeRunHandler() service.Handler {
 		nodeRun, err := workflow.LoadNodeRun(api.mustDB(), key, name, id, workflow.LoadRunOptions{
 			WithTests:           true,
 			WithArtifacts:       true,
-			WithStaticFiles:     true,
 			WithCoverage:        true,
 			WithVulnerabilities: true,
 		})
